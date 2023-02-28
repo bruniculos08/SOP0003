09e0dce9d7ed741c17eb94b5e0ac126496982db8
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2023-02-28 18:55:33 -0300

    update

---
+++ Lista de Exercícios em C/Ex04.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+    int size = atoi(argv[1]);
+    int *vetor = (int *)malloc(sizeof(int) * size);
+    for (int i = 0; i < size; i++){
+
+    }
+}
