63d6a72fc45896a9315e51f85b504cab28a35129
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2023-03-01 11:57:11 -0300

    update

--- Lista de Exercícios em C/Ex04.c
+++ Lista de Exercícios em C/Ex04.c
@@ -3,35 +3,34 @@
 #include <stdlib.h>
 
 // Fila circular (sim eu poderia fazer isso com lista encadeada mas fiz com vetor);
-void printVetor(int *vetor, int size);
+void printVetor(int *vetor, int size, int start);
 
 int main(int argc, char **argv){
     int size = (int) atoi(argv[1]);
     int actualSize = 0;
-    printf("size: %d\n", size);
+    int actualStart = 0;
     int *vetor;
     vetor = (int *)malloc(size*sizeof(int));
     int num = 0;
     for (int i = 0; 1; i++){
         scanf("%i", &num);
-        fflush(stdin);
         if(num == -2){
             i--;
-            printVetor(vetor, actualSize);
-            continue;
+            printVetor(vetor, actualSize, actualStart);
         }
         else if(num == -1) break;
-        else if(num != -2){
+        else{
             vetor[(i%size)] = num;
             if(actualSize < size) actualSize++;
+            else actualStart = (actualStart + 1)%size;
         }
     }
     return 0;
 }
 
-void printVetor(int *vetor, int size){
+void printVetor(int *vetor, int size, int start){
     printf("Fila: ");
-    for (int i = 0; i < size; i++) printf("%i ", *(vetor+i));
+    for (int i = 0; i < size; i++) printf("%i ", *(vetor+(i+start)%size));
     printf("\n");
     return;
 }
---
+++ Lista de Exercícios em C/Ex05.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct PairList
+{
+    char** nameList;
+    int nameListSize;
+    int priority;
+} pair_list;
+
+int main(void){
+    char *nameArg;
+    int priorityArg;
+    pair_list *list = NULL;
+    int listSize = 0;
+    while(1){
+        scanf("%s %d", nameArg, &priorityArg);
+        if(strcmp(nameArg, "MENIG")){}
+        else if(strcmp(nameArg, "FIM")) break;
+        else{
+            
+        }
+    }
+}
+
+void insertPair(pair_list *list, int *size, char *nameArg, int priorityArg){
+    int index = binarySearchVerify(list, size, priorityArg);
+    if(index == -1){
+        // Aumenta o tamanho da lista de pares:
+        *size = *size + 1;
+        // Aloca espaço na lista de pares:
+        realloc(list, (*size) * sizeof(pair_list));
+
+        // Aumenta o tamanho da lista de nomes na nova lista de pares (para 1):
+        list[*size].nameListSize = 1;
+        // Aloca um espaço na lista de nomes:
+        realloc(list[*size].nameList, sizeof(char**));
+
+        // Coloca o nome na primeira posição da lista de nomes (pois de início só há um nome):
+        strcpy(list[*size].nameList[0], nameArg);
+        // Coloca a prioridade na lista de nomes:
+        list[*size].priority = priorityArg;
+    }
+    else{
+        list[index].nameListSize++;
+        realloc(list[*size].nameList, list[*size].nameListSize*sizeof(char**));
+        strcpy(list[index].nameList[list[index].nameListSize], nameArg);
+    }
+}
+
+int binarySearchVerify(pair_list *list, int size, int priority){
+    // Retorna o índice ou se não existe retorna -1 (poderia retorna a posição na qual o item estaria mas...
+    // ... não sei como usaria isto neste código):
+    int index = size/2;
+    while(size != 0){
+        
+    }
+    return -1;
+}
+
+pair_list mergeSort(pair_list *list, int size){
+    pair_list *newList = (pair_list *)malloc(sizeof(pair_list)*size);
+    if(size > 2){
+        
+    }
+    return;
+}
+
+void sort(pair_list *list, int index1, int index2, int end){
+    pair_list tempVetor[end+1];
+    return;
+}
